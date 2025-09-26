c1ca1579cf072d5ecd9fa37adacaab8901a0b061
pankajsharma81 <pankajsharma88279@gmail.com>
2025-09-27 02:39:59 +0530

    updated landing page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,29 +1,32 @@
 import HeroSection from "@/components/Hero";
 import { Button } from "@/components/ui/button";
-import { featuresData, howItWorksData, statsData, testimonialsData } from "@/data/landing";
+import {
+  featuresData,
+  howItWorksData,
+  statsData,
+  testimonialsData,
+} from "@/data/landing";
 import Image from "next/image";
 import { Card, CardContent } from "@/components/ui/card";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="min-h-screen bg-white">
-
-     <HeroSection/>
-     <section className="py-20 bg-blue-50">
-      <div className="container mx-auto px-4">
-        <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
-          {statsData.map((stat,index)=>(
-            <div key={index} className="text-center">
+    <div className="min-h-screen bg-white mt-40">
+      <HeroSection />
+      <section className="py-20 bg-blue-50">
+        <div className="container mx-auto px-4">
+          <div className="grid grid-cols-2 md:grid-cols-4 gap-8">
+            {statsData.map((stat, index) => (
+              <div key={index} className="text-center">
                 <div className="text-4xl font-bold text-blue-600 mb-2">
                   {stat.value}
                 </div>
-                <div className="text-gray-600">{stat.lebel}</div>
-             </div>
-          ))}
-
+                <div className="text-gray-600">{stat.label}</div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>
-     </section>
+      </section>
       <section id="features" className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-12">
@@ -42,7 +45,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-       <section className="py-20 bg-blue-50">
+      <section className="py-20 bg-blue-50">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-16">How It Works</h2>
           <div className="grid grid-cols-1 md:grid-cols-3 gap-12">
@@ -58,7 +61,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-       <section id="testimonials" className="py-20">
+      <section id="testimonials" className="py-20">
         <div className="container mx-auto px-4">
           <h2 className="text-3xl font-bold text-center mb-16">
             What Our Users Say
@@ -89,7 +92,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-       <section className="py-20 bg-blue-600">
+      <section className="py-20 bg-blue-600">
         <div className="container mx-auto px-4 text-center">
           <h2 className="text-3xl font-bold text-white mb-4">
             Ready to Take Control of Your Finances?
